0de4c368419d9e87642d903d581bf54ebef8a24a
Seven <13691146986@163.com>
2019-12-31 22:02:07 +0800

    Update js.go

--- js/js.go
+++ js/js.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/silenceper/wechat/context"
-	"github.com/silenceper/wechat/util"
+	"github.com/sevenMonkey/wechat/context"
+	"github.com/sevenMonkey/wechat/util"
 )
 
 const getTicketURL = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=%s&type=jsapi"
